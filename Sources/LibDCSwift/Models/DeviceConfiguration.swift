c24306f04a6309a0a8edd99eabdf8afce588183e
Latisha <latishabesariani@gmail.com>
2026-07-31 05:45:34 +0800

    chore: drop stale Perdix 3 descriptor TODO
    libdivecomputer picked up the Shearwater Perdix 3 descriptor table
    entry, so find_descriptor_by_model no longer fails for model 14.

--- Sources/LibDCSwift/Models/DeviceConfiguration.swift
+++ Sources/LibDCSwift/Models/DeviceConfiguration.swift
@@ -43,10 +43,6 @@ import LibDCBridge
         ComputerModel(name: "Shearwater Perdix", family: .shearwaterPetrel, modelID: 5),
         ComputerModel(name: "Shearwater Perdix AI", family: .shearwaterPetrel, modelID: 6),
         ComputerModel(name: "Shearwater Perdix 2", family: .shearwaterPetrel, modelID: 11),
-        // TODO: the vendored libdivecomputer doesn't have a descriptor table entry for
-        // model 14 yet (Perdix 3 support is still in progress upstream), so
-        // find_descriptor_by_model will fail and device open/parsing won't work until
-        // libdivecomputer/src/descriptor.c picks up a "Shearwater"/"Perdix 3" entry.
         ComputerModel(name: "Shearwater Perdix 3", family: .shearwaterPetrel, modelID: 14),
         ComputerModel(name: "Shearwater Teric", family: .shearwaterPetrel, modelID: 8),
         ComputerModel(name: "Shearwater Tern", family: .shearwaterPetrel, modelID: 12),
@@ -222,7 +218,7 @@ import LibDCBridge
         CBUUID(string: "ca7b0001-f785-4c38-b599-c7c5fbadb034"), // Pelagic i330R/DSX
         CBUUID(string: "fdcdeaaa-295d-470e-bf15-04217b7aa0a0"), // ScubaPro G2/G3
         CBUUID(string: "fe25c237-0ece-443c-b0aa-e02033e7029d"), // Shearwater Perdix/Teric
-        CBUUID(string: "1aa44039-1667-4b29-87cc-dfecaaf31d97"), // Shearwater Perdix 3 (TODO: no libdivecomputer descriptor entry yet, see supportedModels)
+        CBUUID(string: "1aa44039-1667-4b29-87cc-dfecaaf31d97"), // Shearwater Perdix 3
         CBUUID(string: "0000fcef-0000-1000-8000-00805f9b34fb"), // Divesoft Freedom
         CBUUID(string: "00000001-8c3b-4f2c-a59e-8c08224f3253"), // Halcyon Symbios
         CBUUID(string: "84968ffe-d26d-478a-b953-5010bcf58bca")  // Seac Screen
